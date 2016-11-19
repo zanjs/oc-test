22c6278029eca9b37f1837c0c41719a4856cc015
zanjs <root@zanjs.com>
2016-11-19 10:03:28 +0800

    Initial Commit

---
+++ ocfirst/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ocfirst
+//
+//  Created by Anlasheng on 2016/11/19.
+//  Copyright © 2016年 Anlasheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ocfirst/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ocfirst
+//
+//  Created by Anlasheng on 2016/11/19.
+//  Copyright © 2016年 Anlasheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
