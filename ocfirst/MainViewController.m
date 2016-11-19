8366cef193cc910ce35b1ea2f6391673cabcbe53
zanjs <root@zanjs.com>
2016-11-19 12:21:16 +0800

    父子层级关系

--- ocfirst/MainViewController.m
+++ ocfirst/MainViewController.m
@@ -19,17 +19,27 @@
     NSLog(@"controller load");
     [self.view setBackgroundColor:[UIColor cyanColor]];
     
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-    [view setBackgroundColor:[UIColor orangeColor]];
-    [self.view addSubview:view];
+    UIView *cview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 600, 600)];
+    [cview setBackgroundColor:[UIColor orangeColor]];
+    [self.view addSubview:cview];
     
     UIView *view1 = [[UIView alloc] initWithFrame:CGRectMake(10, 20, 300, 100)];
     [view1 setBackgroundColor:[UIColor purpleColor]];
     
-    UIView *view2 = [[UIView alloc] initWithFrame:CGRectMake(10, 10, 20, 30)];
+    UIView *view2 = [[UIView alloc] initWithFrame:CGRectMake(20, 10, 30, 30)];
     [view2 setBackgroundColor:[UIColor lightGrayColor]];
     [view2 setAlpha:0.6];
     [view1 addSubview:view2];
+    
+    UIView *view3 = [[UIView alloc] initWithFrame:CGRectMake(20, 20, 200, 30)];
+    [view3 setBackgroundColor:[UIColor blueColor]];
+    [view2 addSubview:view3];
+    [view2 setClipsToBounds:YES]; //边界
+    
+    UIView *view4 = [[UIView alloc] initWithFrame:CGRectMake(80, 60, 50, 20)];
+    [view4 setBackgroundColor:[UIColor blackColor]];
+    [cview insertSubview:view4 aboveSubview:view1];
+    [cview insertSubview:view1 atIndex:cview.subviews.count -1];
     
     [self.view addSubview:view1];
 
