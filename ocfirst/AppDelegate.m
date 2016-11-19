27888994adfa36df8a775bd237d66bea171e92e7
zanjs <root@zanjs.com>
2016-11-19 10:42:19 +0800

    :cat: init

--- ocfirst/AppDelegate.m
+++ ocfirst/AppDelegate.m
@@ -17,6 +17,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    [self.window setBackgroundColor:[UIColor whiteColor]];
+    [self.window makeKeyAndVisible];
+    [self.window setRootViewController:[[UIViewController alloc] init]];
     return YES;
 }
 
